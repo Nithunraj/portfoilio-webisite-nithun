98ce5377bf74bf1c10c5ecefe48989900742e159
Nithunraj NM <nithunraj.nm@finsurge.tech>
2025-09-08 14:21:05 +0530

    updated_profile_changes

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Senior Software Developer", "Backend Developer", "Django Developer", "Python Developer", "Senior Software Developer"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+    showCursor: false,
+});
+// <!-- typed js effect ends -->
