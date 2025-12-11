339e56799912464694b55bdc3f8c8408a5ad7b31
Nithunraj <87900221+Nithunraj@users.noreply.github.com>
2025-12-11 15:30:29 +0530

    Add 'Specialist Engineer' to typed strings

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu(){
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Senior Software Developer", "Backend Developer", "Django Developer", "Python Developer", "Senior Software Developer"],
+    strings: ["Senior Software Developer", "Backend Developer", "Django Developer", "Python Developer", "Senior Software Developer","Specialist Engineer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
